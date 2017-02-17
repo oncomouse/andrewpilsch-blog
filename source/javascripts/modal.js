3801e27bb428f3e5c624356302a5f75710fedd48
onocmouse <onocmouse@gmail.com>
2017-02-17 12:00:54 -0600

    Working modals!

--- source/javascripts/modal.js
+++ source/javascripts/modal.js
@@ -1,21 +1,22 @@
+//=require "jquery-modal/jquery.modal"
+
 $(function() {
-	$('img').not('.emoji').each(function() {
-		var modal_id = $(this).attr('src').replace(/^.*\//,'').replace(/\W/g,'');
-		$(this).addClass('modalized').wrap('<div class="modal"></div>').after('<div class="modal-fade-screen"><div class="modal-inner" style="width: '+this.naturalWidth + 'px; height: '+this.naturalHeight + 'px;"><div class="modal-close" for="'+modal_id+'"></div><img src="'+$(this).attr('src')+'"/></div></div>').after($('<input class="modal-state" id="'+modal_id+'" type="checkbox"/>')).wrap('<label for="' + modal_id + '"></label>').wrap('<div class="modal-trigger"></div>');
-	});
-	$('.modal-state').on('change', function() {
-		if ($(this).is(':checked')) {
-			$('body').addClass('modal-open');
-		} else {
-			$('body').removeClass('modal-open');
+	var modal_counter = 0;
+	$('img').not('.emoji').each(
+		function() {
+			var modal_id = "modal_" + modal_counter;
+			modal_counter++;
+			
+			// Polyfill naturalWidth & naturalHeight:
+			if(!('naturalWidth') in new Image()) {
+				var tmp = new Image();
+				tmp.src = this.src;
+				img.naturalWidth = tmp.width;
+				img.naturalHeight = tmp.height;
+			}
+			
+			$(this).addClass('modalized').wrap('<a href="#' + modal_id + '" rel="modal:open" class="modal-trigger"></a>')
+			$('<div id="'+modal_id+'" style="display:none;width:'+(this.naturalWidth + 150)+'px;height:'+(this.naturalHeight+150)+'px;"><div class="modal-inner"><img src="'+$(this).attr('src')+'"/></div></div>').appendTo($('body'));
 		}
-	});
-
-	$('.modal-fade-screen, .modal-close').on('click', function() {
-		$('.modal-state:checked').prop('checked', false).change();
-	});
-
-	$('.modal-inner').on('click', function(e) {
-		e.stopPropagation();
-	});
+	);
 });
