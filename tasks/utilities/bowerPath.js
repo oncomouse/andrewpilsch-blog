ef6220bf018c10016c88e604ad661675fa6c1b52
oncomouse <onocmouse@gmail.com>
2017-06-21 15:34:48 -0500

    Don't need console.log here.

--- tasks/utilities/bowerPath.js
+++ tasks/utilities/bowerPath.js
@@ -2,7 +2,6 @@ import path from 'path'
 import {execSync} from 'child_process'
 
 export default () => {
-	console.log()
 	return JSON.parse(execSync(`${path.resolve(path.join(
 	'.',
 	'node_modules',
